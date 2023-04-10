a5240fc4e8a72894bb7fb94101f8eb05bb4892ee
John Doe <juangnba@hotmail.com>
2023-04-10 15:13:28 +0200

    Boolean and array tests

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,24 @@
 const text = "Hello world";
+const fruits = ["apple", "orange", "banana"];
 
-test("Must contain a string", () => {
-    expect(text).toMatch(/world/);
+test("Contains banana?", () => {
+    expect(fruits).toContain("banana");
+});
+
+test("Greater than", () => {
+    expect(10).toBeGreaterThan(9);
+});
+
+test("True", () => {
+    expect(true).toBeTruthy();
+});
+
+const reverseString = (str, callback) => {
+    callback(str.split("").reverse().join(""));
+};
+
+test("Callback test", () => {
+    reverseString("Hello", (str) => {
+        expect(str).toBe("olleH");
+    });
 });
