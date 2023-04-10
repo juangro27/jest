08794a489e0c6cfac0bb291424dbe9e1b1b0426f
John Doe <juangnba@hotmail.com>
2023-04-10 15:19:45 +0200

    Promises testing

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -22,3 +22,29 @@ test("Callback test", () => {
         expect(str).toBe("olleH");
     });
 });
+
+const reverseString2 = (str) => {
+    return new Promise((resolve, reject) => {
+        if (!str) {
+            reject(Error("Error"));
+        }
+        resolve(str.split("").reverse().join(""));
+    });
+};
+
+test("Promise test", () => {
+    return reverseString2("Hello").then((str) => {
+        expect(str).toBe("olleH");
+    });
+});
+
+test("Async/Await test", async () => {
+    const str = await reverseString2("Hello");
+    expect(str).toBe("olleH");
+});
+
+afterEach(() => console.log("After each test"));
+afterAll(() => console.log("After all tests"));
+
+beforeEach(() => console.log("Before each test"));
+beforeAll(() => console.log("Before all tests"));
